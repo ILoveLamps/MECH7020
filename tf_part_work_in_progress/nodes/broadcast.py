9fc1cae534c0d72e9bf70afe67b912aa103f43c5
ILoveLamps <60797811+ILoveLamps@users.noreply.github.com>
2020-04-21 15:15:45 -0400

    V3 - tf works but numbers don't make any sense

--- tf_part_work_in_progress/nodes/broadcast.py
+++ tf_part_work_in_progress/nodes/broadcast.py
@@ -3,11 +3,10 @@
     04.20.20
     Broadcast Node
         1. Camera Feed Frame (april-tag detection)
-        2. TurtleBot3 Frame (odometry) 
+        2. TurtleBot3 Frame (odometry)
     - Hirdayesh Shrestha
     - Matthew Postell
     - Dhruv Patel
-
 """
 import roslib
 import rospy
@@ -33,17 +32,16 @@ from geometry_msgs.msg import (
     Quaternion,
 )
 
-
 roslib.load_manifest('ttbot_waypoint')
 
 
-def find_marker(detections,marker_num):
+def find_marker(detections, marker_num):
     """
     Finds index of marker #marker_num in the apriltag detection message detections. If not found,
     returns -1.
     """
-    for i in range(0,len(detections)):
-        if detections[i].id==marker_num:
+    for i in range(0, len(detections)):
+        if detections[i].id == marker_num:
             return i
     return -1
 
@@ -53,59 +51,54 @@ def get_marker(detections):
     Parses an apriltag detection message and gets the index for the last used marker, or
     simply the first one found if the last one was not seen. Returns -1 if nothing found.
     """
-    if (len(detections)==0):
-        return None,None
+    if (len(detections) == 0):
+        return None, None
     last_marker = -1
-    marker = find_marker(detections,last_marker)
-    if (marker!=-1):
-        return last_marker,marker
+    marker = find_marker(detections, last_marker)
+    if (marker != -1):
+        return last_marker, marker
     else:
-        last_marker=detections[0].id
-        return detections[0].id,0
-
-
+        last_marker = detections[0].id
+        return detections[0].id, 0
 
 
 def get_t_R(pose):
     """
     Returns the translation vector (4x1) and rotation matrix (4x4) from a pose message
     """
-    t=np.transpose(np.matrix([pose.pose.position.x,pose.pose.position.y,pose.pose.position.z,1]))
-    quat=[pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z, pose.pose.orientation.w]
-    R_full=quaternion_matrix(quat)
-    R=R_full
-    return t,R
+    t = np.transpose(np.matrix([pose.pose.position.x, pose.pose.position.y, pose.pose.position.z, 1]))
+    quat = [pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z, pose.pose.orientation.w]
+    R_full = quaternion_matrix(quat)
+    R = R_full
+    return t, R
+
 
 def handle_tag_pose(msg):
     br = tf.TransformBroadcaster()
-    # Function to get the pose from msg 
-    (my_marker,marker_pos) = get_marker(msg.detections)
-
-    (marker_t, marker_R)= get_t_R(msg.detections[marker_pos].pose.pose)	    
-
-    print("TAG : \n\n", marker_t)		
+    # Function to get the pose from msg
+    (my_marker, marker_pos) = get_marker(msg.detections)
+    (marker_t, marker_R) = get_t_R(msg.detections[marker_pos].pose.pose) # throws an error for marker_pos as noneType
+    # print("TAG : \n {} \n".format(marker_t))
     br.sendTransform((marker_t[0], marker_t[1], 0),
                      tf.transformations.quaternion_from_euler(0, 0, marker_t[2]),
                      rospy.Time.now(),
-		             "my_marker",
+                     "my_marker",
                      "world")
 
 
 def handle_bot_pose(msg):
     br = tf.TransformBroadcaster()
 
-    pose = msg.pose.pose	    
-    print("BOT : \n\n", pose)		
+    pose = msg.pose.pose
+    # print("BOT :  \n {} \n".format(pose))
     br.sendTransform((pose.position.x, pose.position.y, 0),
                      tf.transformations.quaternion_from_euler(0, 0, pose.position.z),
                      rospy.Time.now(),
-		             "odom",
+                     "odom",
                      "world")
 
 
-
 if __name__ == '__main__':
-
     # ******************************
     #       Main Program Loop
     # ******************************
@@ -114,13 +107,18 @@ if __name__ == '__main__':
 
     global last_marker
     last_marker = -1
-    
-    rospy.Subscriber('/tag_detections',
-                     AprilTagDetectionArray,
-                     handle_tag_pose)
-    
-    rospy.Subscriber('/odom',
-                     Odometry,
-                     handle_bot_pose)
-	
-    rospy.spin()
+
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        try:
+            rospy.Subscriber('/tag_detections',
+                             AprilTagDetectionArray,
+                             handle_tag_pose)
+
+            rospy.Subscriber('/odom',
+                             Odometry,
+                             handle_bot_pose)
+            print("broadcasting")
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+        rate.sleep()
--- tf_part_work_in_progress/nodes/listen.py
+++ tf_part_work_in_progress/nodes/listen.py
@@ -5,33 +5,31 @@
     - Hirdayesh Shrestha
     - Matthew Postell
     - Dhruv Patel
-
 """
 
 import roslib
 import rospy
-import math
 import tf
-import geometry_msgs.msg
-from nav_msgs.msg import Odometry
 
 roslib.load_manifest('ttbot_waypoint')
-
 
 if __name__ == '__main__':
 
     # ******************************
     #       Main Program Loop
     # ******************************
-    
+
     rospy.init_node('listen')
 
     listener = tf.TransformListener()
-    rate = rospy.Rate(10.0)
 
+    rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
         try:
-            (trans,rot) = listener.lookupTransform('odom', 'my_marker', rospy.Time(0))
+            listener.waitForTransform('base_footprint', 'my_marker', rospy.Time(), rospy.Duration(5.0))
+            (trans, rot) = listener.lookupTransform('base_footprint', 'my_marker', rospy.Time())
+            print("Translation :  \n {} \n".format(trans))
+            print("Rotation :  \n {} \n".format(rot))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
 
